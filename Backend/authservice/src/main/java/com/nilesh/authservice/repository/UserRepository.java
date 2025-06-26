b3fd670374a458358bc8d96503c31d6e103a0ca2
Nilesh Gorade <nileshgorade2004@gmail.com>
2025-06-26 17:46:06 +0530

    feat(repo) add interface repository

---
+++ Backend/authservice/src/main/java/com/nilesh/authservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.nilesh.authservice.repository;
+
+import com.nilesh.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
