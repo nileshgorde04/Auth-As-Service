f87f7af8f204775eecb9956c738aac7d05476486
Nilesh Gorade <nileshgorade2004@gmail.com>
2025-07-15 16:56:05 +0530

    fix issue in loading

--- Backend/authservice/src/main/java/com/nilesh/authservice/oauth2/CustomOAuth2UserService.java
+++ Backend/authservice/src/main/java/com/nilesh/authservice/oauth2/CustomOAuth2UserService.java
@@ -1,5 +1,6 @@
 package com.nilesh.authservice.oauth2;
 
+import com.nilesh.authservice.model.CustomOAuth2User;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -11,10 +12,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-
-        // Here you can extract user info and save/update in DB if needed
-        // Example: String email = oAuth2User.getAttribute("email");
-
-        return oAuth2User;
+        return new CustomOAuth2User(oAuth2User);
     }
 }
+
